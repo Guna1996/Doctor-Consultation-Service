7369996f5ee1b6982a30ef713575915465b9404d
BalaAshwanth <balaashwanth.natarajan@ideas2it.com>
2022-10-19 10:33:39 +0530

    new methods

--- src/main/java/com/ideas2it/healthCare/repo/DoctorClinicRepository.java
+++ src/main/java/com/ideas2it/healthCare/repo/DoctorClinicRepository.java
@@ -17,7 +17,5 @@ public interface DoctorClinicRepository extends JpaRepository<DoctorClinic, Inte
 
     Boolean existsByIdAndStatus(int id, String status);
 
-    @Query(value = "Select * from doctor_clinic dc where dc.doctor_id = ?1 and dc.clinic_id = ?2 and status = ?3",
-            nativeQuery = true)
-    Optional<DoctorClinic> findBlyDoctorIdAndClinicIdAndStatus(int doctorId, int clinicId, String status);
+    Optional<DoctorClinic> findByDoctorIdAndClinicIdAndStatus(int doctorId, int clinicId, String status);
 }
