238a3bc8c08ab0efd87449f9a35d7a01e5f4f9eb
BalaAshwanth <balaashwanth.natarajan@ideas2it.com>
2022-11-02 17:41:15 +0530

    final changes

--- src/main/java/com/ideas2it/healthcare/util/MathUtil.java
+++ src/main/java/com/ideas2it/healthcare/util/MathUtil.java
@@ -1,7 +1,33 @@
+/**
+ * <p>
+ * This is the base package for all the util classes
+ * which is for simple calculations
+ * </p>
+ * Copyright 2022 - Ideas2it
+ */
 package com.ideas2it.healthcare.util;
 
+/**
+ * <p>
+ * MathUtil class is used to do simple maths
+ * calculations.
+ * </p>
+ *
+ * @author Bala Ashwanth
+ * @version 1
+ * @since 2022-10-10
+ */
 public class MathUtil {
 
+    /**
+     * <p>
+     * This method is used to get round of
+     * value of two numbers.
+     * </p>
+     * @param count {@link Float}
+     * @param rows {@link Float}
+     * @return {@link Integer}
+     */
     public static Integer getExactCount(float count, float rows) {
         float result =  count/rows;
         if (Math.floor(result) < result) {
