ed95a532c59d87192d15136850fe6cc72542a4e2
BalaAshwanth <balaashwanth.natarajan@ideas2it.com>
2022-11-07 12:39:54 +0530

    yes done

--- src/main/java/com/ideas2it/healthcare/service/impl/DoctorClinicServiceImpl.java
+++ src/main/java/com/ideas2it/healthcare/service/impl/DoctorClinicServiceImpl.java
@@ -51,7 +51,8 @@ public class DoctorClinicServiceImpl implements DoctorClinicService {
      * {@inheritDoc}
      */
     public String assignDoctorToClinic(DoctorClinicDto doctorClinicDto) {
-        if (!isDoctorAvailable(doctorClinicDto.getDoctor().getId(), doctorClinicDto.getTimeslots())) {
+        if (!isDoctorAvailable(doctorClinicDto.getDoctor().getId(), doctorClinicDto.getClinic().getId(),
+                doctorClinicDto.getTimeslots())) {
             throw new NotFoundException(ErrorConstants.DOCTOR_ALREADY_ASSIGNED_TO_THIS_CLINIC);
         } else {
             try {
@@ -66,17 +67,19 @@ public class DoctorClinicServiceImpl implements DoctorClinicService {
     /**
      * {@inheritDoc}
      */
-    private boolean isDoctorAvailable(int doctorId, List<TimeslotDto> timeslotsDto) {
+    private boolean isDoctorAvailable(int doctorId, int clinicId, List<TimeslotDto> timeslotsDto) {
         List<DoctorClinic> doctorClinics = doctorClinicRepository
                 .findByDoctorIdAndStatus(doctorId, Constants.ACTIVE);
         if (!doctorClinics.isEmpty()) {
             for (TimeslotDto timeslotDto: timeslotsDto) {
                 for (DoctorClinic doctorClinic : doctorClinics) {
-                    List<Timeslot> timeslots = doctorClinic.getTimeslots();
-                    for (Timeslot timeslot : timeslots) {
-                        if (timeslot.getId() == timeslotDto.getId())
-                            throw new NotFoundException(
-                                    ErrorConstants.DOCTOR_ALREADY_ASSIGNED_TO_SOME_OTHER_CLINIC);
+                    if(doctorClinic.getClinic().getId() != clinicId) {
+                        List<Timeslot> timeslots = doctorClinic.getTimeslots();
+                        for (Timeslot timeslot : timeslots) {
+                            if (timeslot.getId() == timeslotDto.getId())
+                                throw new NotFoundException(
+                                        ErrorConstants.DOCTOR_ALREADY_ASSIGNED_TO_SOME_OTHER_CLINIC);
+                        }
                     }
                 }
             }
